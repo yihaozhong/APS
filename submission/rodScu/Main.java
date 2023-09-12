74cf1bab5415455e6d5449116a35718ae60f35ff
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-09-11 20:07:31 -0400

    hw1

---
+++ submission/rodScu/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String ori = "+x";
+        String[] arr = sc.nextLine().split(" ");
+        for (int i = 0; i <n-1; i++){
+            String next = arr[i];
+            
+            if (next.equals("No")){
+                continue;
+            }else if (ori.equals("+x")){
+                ori = next;
+                continue;
+            }else if (ori.equals("-x")) {
+                ori  = (next.charAt(0) == '+' ? "-" : "+") + next.charAt(1);
+                continue;
+            }else if (next.charAt(1)== 'y'){
+                if (ori.charAt(1) == 'y'){
+                    if (ori.charAt(0) == next.charAt(0))
+                        ori = "-x";
+                    else
+                        ori = "+x";
+                }
+            }else if (next.charAt(1) == 'z'){
+                if (ori.charAt(1) == 'z'){
+                    if (ori.charAt(0) == next.charAt(0))
+                        ori = "-x";
+                    else
+                        ori = "+x";
+                }
+            }
+        }
+
+        System.out.print(ori);
+        sc.close();
+    }
+}
