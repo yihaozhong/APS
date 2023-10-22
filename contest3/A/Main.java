27cede10d92b28d9b2dd463feff4d44d589a8fc3
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-10-22 17:22:25 -0400

    prepare for mitderm

--- contest3/A/Main.java
+++ contest3/A/Main.java
@@ -1,37 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-    private static int getDistance(int num1, int num2) {
-        if (num1 == 0)
-            num1 = 10;
-        if (num2 == 0)
-            num2 = 10;
-        return Math.abs(num1 - num2);
-
-    }
-
-    private static int getPinCodeTime(String pinCode) {
-        int totalTime = 0;
-        int prevDigit = '1' - '0'; // Starting position
-        for (char c : pinCode.toCharArray()) {
-            int currentDigit = c - '0';
-            totalTime += getDistance(prevDigit, currentDigit) + 1; // +1 for the press time
-            prevDigit = currentDigit;
-        }
-        return totalTime;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
-        sc.nextLine(); // consume the newline character
-
-        for (int i = 0; i < t; i++) {
-            String pin = sc.nextLine();
-
-            System.out.println(getPinCodeTime(pin));
+        while (t-- > 0) {
+            String pin = sc.next();
+            int time = 0;
+            for (int i = 0; i < pin.length(); i++) {
+                int digit = pin.charAt(i) - '0';
+                if (digit == 0)
+                    digit = 10;
+                if (i == 0) {
+                    time += Math.abs(digit - 1);
+                } else {
+                    int prevDigit = pin.charAt(i - 1) - '0';
+                    if (prevDigit == 0)
+                        prevDigit = 10;
+                    time += Math.abs(digit - prevDigit);
+                }
+            }
+            time += pin.length();
+            System.out.println(time);
         }
+        sc.close();
     }
 }
--- practiceMidterm/poppi/Main.java
+++ practiceMidterm/poppi/Main.java
@@ -1,57 +1,99 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+// import java.util.Scanner;
+
+// public class Main {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int numCases = sc.nextInt();
+// int[] rungs = new int[numCases + 1];
+// rungs[0] = 0;
+// for (int i = 1; i <= numCases; i++) {
+// rungs[i] = sc.nextInt();
+// }
+
+// int[] diffs = new int[numCases];
+// for (int i = 1; i < numCases + 1; i++) {
+// diffs[i - 1] = rungs[i] - rungs[i - 1];
+// }
+
+// // int k = Arrays.stream(diffs).max().getAsInt();
+// // boolean flag = true;
+// // int cnt = 0;
+// // while (flag) {
+// // flag = false;
+// // cnt = 0;
+// // int temp_k = k;
+// // for (int diff : diffs) {
+// // // System.out.println("diff: " + diff + " k: " + k);
+// // if (diff == k) {
+// // k--;
+// // cnt++;
+// // if (k <= 0) {
+// // k = temp_k + 1;
+// // flag = true;
+// // }
+// // } else if (diff > k) {
+// // k = temp_k + 1;
+// // cnt = 0;
+// // flag = true;
+// // break;
+// // }
+// // }
+// // }
+// int k = diffs[numCases - 1];
+// for (int i = numCases - 2; i >= 0; i--) {
+// if (diffs[i] > k) {
+// k = diffs[i];
+// } else if (diffs[i] == k) {
+// k++;
+// }
+// }
+
+// // System.out.println("diff: " + diff + " k: " + k);
+
+// System.out.println(k);
+// }
+// }
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int numCases = sc.nextInt();
-        int[] rungs = new int[numCases + 1];
-        rungs[0] = 0;
-        for (int i = 1; i <= numCases; i++) {
+        int n = sc.nextInt();
+        int[] rungs = new int[n];
+        for (int i = 0; i < n; i++) {
             rungs[i] = sc.nextInt();
         }
 
-        int[] diffs = new int[numCases];
-        for (int i = 1; i < numCases + 1; i++) {
-            diffs[i - 1] = rungs[i] - rungs[i - 1];
-        }
+        System.out.println(minK(rungs));
+    }
 
-        // int k = Arrays.stream(diffs).max().getAsInt();
-        // boolean flag = true;
-        // int cnt = 0;
-        // while (flag) {
-        // flag = false;
-        // cnt = 0;
-        // int temp_k = k;
-        // for (int diff : diffs) {
-        // // System.out.println("diff: " + diff + " k: " + k);
-        // if (diff == k) {
-        // k--;
-        // cnt++;
-        // if (k <= 0) {
-        // k = temp_k + 1;
-        // flag = true;
-        // }
-        // } else if (diff > k) {
-        // k = temp_k + 1;
-        // cnt = 0;
-        // flag = true;
-        // break;
-        // }
-        // }
-        // }
-        int k = diffs[numCases - 1];
-        for (int i = numCases - 2; i >= 0; i--) {
-            if (diffs[i] > k) {
-                k = diffs[i];
-            } else if (diffs[i] == k) {
-                k++;
+    private static int minK(int[] rungs) {
+        int lo = 0;
+        int hi = 10000000;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (canReachTop(rungs, mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
             }
         }
+        return lo;
+    }
 
-        // System.out.println("diff: " + diff + " k: " + k);
-
-        System.out.println(k);
+    private static boolean canReachTop(int[] rungs, int k) {
+        int lastRung = 0;
+        for (int rung : rungs) {
+            if (rung - lastRung > k) {
+                return false;
+            } else if (rung - lastRung == k) {
+                k--;
+            }
+            lastRung = rung;
+        }
+        return true;
     }
 }
