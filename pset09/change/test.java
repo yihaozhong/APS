f6c9985b30bdfafa89c1b95a1b31918df754cb51
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-11-12 22:12:49 -0500

    pset09 continued

--- pset09/change/test.java
+++ pset09/change/test.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Main {
+public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt(); 
+        int M = sc.nextInt(); // Price of the garage kit
+        int N = sc.nextInt(); // Number of coins
         int[] coins = new int[N];
         for (int i = 0; i < N; i++) {
             coins[i] = sc.nextInt();
         }
         sc.close();
 
-        int maxCoinValue = Arrays.stream(coins).max().getAsInt();
-        int[] dp = new int[M + maxCoinValue + 1];
+        // Initialize DP array. Set high values to represent "infinity".
+        int[] dp = new int[M + 10001]; // We only need to go up to M + largest coin value, but 10001 to be safe
         Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        dp[0] = 0; // Base case: 0 coins needed to pay 0 amount
 
-        // Populate the dp array with the minimum number of coins needed for each sum
+        // Update dp array for all coins
         for (int coin : coins) {
-            for (int j = dp.length - 1; j >= coin; j--) {
+            for (int j = coin; j < dp.length; j++) {
                 if (dp[j - coin] != Integer.MAX_VALUE) {
                     dp[j] = Math.min(dp[j], dp[j - coin] + 1);
                 }
@@ -27,17 +27,11 @@ public class Main {
         }
 
         // Find the minimum overpayment and number of coins used
-        int totalPaid = -1;
-        int coinsUsed = Integer.MAX_VALUE;
         for (int i = M; i < dp.length; i++) {
-            if (dp[i] < coinsUsed) {
-                totalPaid = i;
-                coinsUsed = dp[i];
+            if (dp[i] != Integer.MAX_VALUE) {
+                System.out.println(i + " " + dp[i]);
                 break;
             }
         }
-    
-
-        System.out.println(totalPaid + " " + coinsUsed);
     }
 }
